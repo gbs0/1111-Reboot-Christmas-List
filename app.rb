e8aae92d63b6d7029b3b802a270af84ccc1c65c7
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-02-04 15:44:03 -0300

    automatted commit

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 # É um objeto que representa cada presente
 # gift = {name: "Playstation 5", price: 4999}
 # É uma lista com todos os presentes
@@ -5,8 +7,50 @@ GIFTS = []
 
 def add(gift)
   GIFTS.push(gift)
+  save
 end
 
 def all_gifts
   return GIFTS
+end
+
+def find(index)
+  GIFTS[index]
+end
+
+def destroy(index)
+  GIFTS.delete_at(index)
+  save
+end
+
+def mark_gift(index)
+  # 1. A partir do index, procurar na lista qual é o presente
+  # 2. Marca o presente como comprado ( bought = 1 )
+  # 3. Salva o CSV
+  gift = find(index)
+  # gift = {name: "Playstation 5", price: 4999, bought: 0}
+  gift[:bought] = 1
+  # gift = {name: "Playstation 5", price: 4999, bought: 1}
+  save
+end
+
+def load
+  CSV.foreach('gifts.csv', headers: :first_row) do |row|
+    # row = ["Playstion 5", "999", "0"]
+    # row = {"name" => "Playstation 5", "price" => "999", "bought": "0"}
+    gift = {name: row['name'], price: row['price'].to_i, bought: row['bought'].to_i}
+    add(gift)
+  end
+end
+
+def save
+  # 1. Abrir o CSV em modo de escrita 
+  # 2. Cria os headers dentro do CSV
+  # 3. Iterar sobre os presentes que estão na MEMORIA (GIFTS)
+  CSV.open('gifts.csv', 'wb') do |file|
+    file << ['name', 'price', 'bought']
+    GIFTS.each do |gift|
+      file << [gift[:name], gift[:price], gift[:bought]]
+    end
+  end
 end
--- interface.rb
+++ interface.rb
@@ -5,6 +5,7 @@
 require_relative 'app'
 
 action = 1
+load if File.exist?('gifts.csv')
 
 def print_menu
   puts '🎄🎅🏼'* 5
@@ -33,12 +34,38 @@ def list_gifts
   end
 end
 
+def delete_gifts
+  # 1. Lista todos os itens disponiveis
+  list_gifts
+  # 2. Perguntar ao usuário qual o index a ser deletado
+  puts "What's the item index you want to remove?"
+  index_input = gets.chomp.to_i - 1
+  # 3. Retiramos da array atráves do index
+  destroy(index_input)
+end
+
+def mark_gifts
+  # 1. Listar todos os itens disponiveis
+  # 2. Perguntar ao usuário qual o index a ser marcado
+  # 3. Atráves do indice, chamamos o método mark_gift(index_input)
+  list_gifts
+  puts "What's the index you want to mark as bought?"
+  index_input = gets.chomp.to_i - 1
+  mark_gift(index_input)
+end
+
 def dispatch(action_number)
   case action_number
   when 1
     list_gifts
   when 2
     add_gifts
+  when 3
+    delete_gifts
+  when 5
+    mark_gifts
+  when 0
+    exit
   else
     puts "Invalid option, Try again!"
   end
